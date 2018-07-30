6860d994f5dad40a57815dca445a70d8cc31afd7
sallvador <crea1323tive>
2018-07-30 23:54:00 +0300

    Created metric for competition
    It's really dumb

--- saltsegm/metrics.py
+++ saltsegm/metrics.py
@@ -8,3 +8,20 @@ def fraction(numerator, denominator, empty_val: float = 1):
 
 def dice_score(x: np.ndarray, y: np.ndarray) -> float:
     return fraction(2 * np.sum(x & y), np.sum(x) + np.sum(y))
+
+def main_metrics(x: np.ndarray, y: np.ndarray) -> float:
+    assert x.shape == y.shape
+    if np.sum(x | y) == 0:
+        return 1
+    elif (np.sum(x) == 0) | (np.sum(y) == 0):
+        return 0
+    else:
+        iou = np.sum(x & y) / np.sum(x | y)
+        threshholds = np.arange(0.5, 1, 0.05)
+        metric = 0
+        for th in threshholds:
+            TP = int(th < iou)
+            FN = int(np.sum(x & y) < np.sum(y))
+            FP = int(np.sum(x & y) < np.sum(x))
+            metric += TP / (TP + FN + FP)
+        return metric / threshholds.shape[0]
