fc057f12ec1d4fc27010cd3007e369ff48c8aaa7
Boris <bg.dustpelt@gmail.com>
2018-08-18 17:10:34 +0300

    changed load_experiment_data to load_val_data

--- saltsegm/experiment.py
+++ saltsegm/experiment.py
@@ -38,8 +38,8 @@ def generate_experiment(exp_path, cv_splits, dataset):
         dump_json(list(np.array(split['test_ids'], dtype='str')), os.path.join(val_path, 'test_ids.json'))
 
 
-def load_experiment_data(exp_path, n_val):
-    """Loads stacks of images to carry on experiment with.
+def load_val_data(exp_path, n_val):
+    """Loads stacks of images to validate model on.
 
     Parameters
     ----------
@@ -51,7 +51,7 @@ def load_experiment_data(exp_path, n_val):
 
     Returns
     -------
-        x_train, y_train, x_val, y_val, x_test, y_test: np.ndarray
+        x_val, y_val: np.ndarray
     """
     config_path = os.path.join(exp_path, 'config.json')
 
@@ -61,16 +61,7 @@ def load_experiment_data(exp_path, n_val):
 
     val_path = os.path.join(exp_path, f'experiment_{n_val}')
 
-    train_ids = np.array(load_json(os.path.join(val_path, 'train_ids.json')), dtype='int64')
     val_ids = np.array(load_json(os.path.join(val_path, 'val_ids.json')), dtype='int64')
-    test_ids = np.array(load_json(os.path.join(val_path, 'test_ids.json')), dtype='int64')
-
-    x_train, y_train = [], []
-    for _id in train_ids:
-        x_train.append(ds.load_x(_id))
-        y_train.append(ds.load_y(_id))
-    x_train = np.array(x_train, dtype='float32')
-    y_train = np.array(y_train, dtype='float32')
 
     x_val, y_val = [], []
     for _id in val_ids:
@@ -79,14 +70,7 @@ def load_experiment_data(exp_path, n_val):
     x_val = np.array(x_val, dtype='float32')
     y_val = np.array(y_val, dtype='float32')
 
-    x_test, y_test = [], []
-    for _id in test_ids:
-        x_test.append(ds.load_x(_id))
-        y_test.append(ds.load_y(_id))
-    x_test = np.array(x_test, dtype='float32')
-    y_test = np.array(y_test, dtype='float32')
-
-    return x_train, y_train, x_val, y_val, x_test, y_test
+    return x_val, y_val
 
 
 def make_predictions(exp_path, n_val, model):
@@ -172,3 +156,17 @@ def calculate_metrics(exp_path, n_val, metrics_dict):
         metric_filename = os.path.join(metric_path, metric_name + '.json')
         dump_json(results, metric_filename)
     # end for
+
+
+def get_experiment_result(exp_path, n_splits, metric_name):
+    val_results = []
+    
+    for i in range(n_splits):
+        metric_path = os.path.join(EXP_PATH, f'experiment_{i}/test_metrics/{metric_name}.json')
+        results_dict = load_json(metric_path)
+
+        val_mean = np.mean(list(results_dict.values()))
+        val_results.append(val_mean)
+    # end for
+    
+    return np.mean(val_results)
