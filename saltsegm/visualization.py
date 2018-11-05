1b5793c688e64f6fd854f06135f551aa00930ff7
Leonid Litovchenko <litleo@mail.ru>
2018-11-05 17:32:11 +0300

    ENH: re-worked visualization.plot_metrics() using plt.subplots()

--- saltsegm/visualization.py
+++ saltsegm/visualization.py
@@ -32,7 +32,7 @@ def get_points_of_interest(exp_path: str, n_val: int):
 
 
 # TODO exp_path should contain folders experiment_N, which are to be parsed
-def plot_metrics(exp_path: str, n_val: int, highlight=True):
+def plot_metrics(exp_path: str, n_val: int, fig_size=(15, 12), highlight=True):
     """
     Plots validation loss, validation metrics and learning rates
         of given n_val split over epochs.
@@ -45,34 +45,38 @@ def plot_metrics(exp_path: str, n_val: int, highlight=True):
     n_val : int
         Validation split number, required to access experiment_{n_val} folder
 
+    fig_size : (int, int), optional
+        Size of canvas for plots.
+
     highlight : bool, optional
         Put the max val metrics and min loss points of interest on plot
     """
-    if highlight:
-        max_metric, min_loss = get_points_of_interest(exp_path, n_val)
+    fig, ax = plt.subplots(3, 1, figsize=fig_size)
 
     log = load_log(exp_path, n_val)
     val_losses = log['val_losses']
     val_metrics = log['val_metrics']
     val_lrs = log['val_lrs']
 
-    fig = plt.figure(figsize=(15, 12))
-    plt.subplot(3, 1, 1)
-    if highlight:
-        plt.axvline(min_loss[0], linestyle='--', color='green')
-        plt.axhline(min_loss[1], linestyle='--', color='green')
-        plt.plot(min_loss[0], min_loss[1], marker='o', markersize=8, color="red")
-    plt.plot(val_losses)
-    plt.title('Val losses', fontsize=20)
+    # CAREFUL WITH FONTSIZE! Labels may overlap!
+    ax[0].set_title('Val losses', fontsize=20)
+    ax[0].plot(val_losses)
 
-    plt.subplot(3, 1, 2)
-    if highlight:
-        plt.axvline(max_metric[0], linestyle='--', color='cyan')
-        plt.axhline(max_metric[1], linestyle='--', color='cyan')
-        plt.plot(max_metric[0], max_metric[1], marker='o', markersize=8, color="red")
-    plt.plot(val_metrics)
-    plt.title('Val metrics', fontsize=20)
+    ax[1].set_title('Val metrics', fontsize=20)
+    ax[1].plot(val_metrics)
 
-    plt.subplot(3, 1, 3)
-    plt.plot(val_lrs)
-    plt.title('Learning rates', fontsize=20)
+    ax[2].set_title('Learning rates', fontsize=20)
+    ax[2].plot(val_lrs)
+
+    if highlight:
+        max_metric, min_loss = get_points_of_interest(exp_path, n_val)
+
+        ax[0].axvline(min_loss[0], linestyle='--', color='cyan')
+        ax[0].axhline(min_loss[1], linestyle='--', color='cyan')
+        ax[0].plot(min_loss[0], min_loss[1], marker='o', markersize=8, color="red")
+
+        ax[1].axvline(max_metric[0], linestyle='--', color='cyan')
+        ax[1].axhline(max_metric[1], linestyle='--', color='cyan')
+        ax[1].plot(max_metric[0], max_metric[1], marker='o', markersize=8, color="red")
+
+    plt.plot()
